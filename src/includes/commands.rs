92c75e5d1a04ee33cfebd4905538672f2a77de9e
Sen ZmaKi <phidelalvin@gmail.com>
2023-12-10 06:07:48 +0300

    feat: Add download_installer command endpoint (untested)

--- src/includes/commands.rs
+++ src/includes/commands.rs
@@ -13,6 +13,8 @@ use crate::includes::{
     utils::LoadingAnimation,
 };
 
+use super::install::Installer;
+
 async fn find_repo(name: &str, client: &Client) -> Result<Option<Repo>, reqwest::Error> {
     let name_lower = name.to_lowercase();
     let mut found_repo: Option<Repo> = None;
@@ -40,6 +42,36 @@ pub async fn show_package(
     }
 }
 
+pub async fn download_installer(
+    name: &str,
+    client: &Client,
+    version: &str,
+    version_regex: &Regex,
+    download_path: &PathBuf,
+) -> Result<Option<(Repo, Installer, PathBuf)>, KnownErrors> {
+    match find_repo(name, client).await? {
+        Some(repo) => {
+            let installer = match version {
+                "latest" => repo.get_latest_installer(client, version_regex).await?,
+                version => repo.get_installer(client, version, version_regex).await?,
+            };
+            match installer {
+                Some(installer) => {
+                    let installer_path = installer.download(download_path, client).await?;
+                    Ok(Some((repo, installer, installer_path)))
+                }
+                None => {
+                    println!("Couldn't find a valid installer for {}", name);
+                    Ok(None)
+                }
+            }
+        }
+        None => {
+            println!("Couldn't find a package named \"{}\"", name);
+            Ok(None)
+        }
+    }
+}
 pub async fn install_package(
     db: &mut PackageDBManager,
     name: &str,
@@ -54,32 +86,24 @@ pub async fn install_package(
 ) -> Result<(), KnownErrors> {
     match db.find_package(name)? {
         Some(package) => Ok(println!("{}\nPackage is already installed", package)),
-        None => match find_repo(name, client).await? {
-            Some(repo) => {
-                let installer = match version {
-                    "latest" => repo.get_latest_installer(client, version_regex).await?,
-                    version => repo.get_installer(client, version, version_regex).await?,
-                };
-                match installer {
-                    Some(installer) => {
-                        let installer_path = installer.download(download_path, client).await?;
-                        let install_info = installer.install(
-                            &installer_path,
-                            loading_animation,
-                            startmenu_folder,
-                            user_uninstall_reg_key,
-                            machine_uninstall_reg_key,
-                        )?;
-                        let package_name = repo.name.to_owned();
-                        let package = Package::new(installer.version, repo, install_info);
-                        db.add_package(package)?;
-                        Ok(println!("Succesfully installed {}", package_name))
-                    }
-                    None => Ok(println!("Couldn't find a valid installer for the package")),
+        None => {
+            match download_installer(name, client, version, version_regex, download_path).await? {
+                Some((repo, installer, installer_path)) => {
+                    let install_info = installer.install(
+                        &installer_path,
+                        loading_animation,
+                        startmenu_folder,
+                        user_uninstall_reg_key,
+                        machine_uninstall_reg_key,
+                    )?;
+                    let package_name = repo.name.to_owned();
+                    let package = Package::new(installer.version, repo, install_info);
+                    db.add_package(package)?;
+                    Ok(println!("Succesfully installed {}", package_name))
                 }
+                None => Ok(()),
             }
-            None => Ok(println!("Couldn't find a package named \"{}\"", name)),
-        },
+        }
     }
 }
 
